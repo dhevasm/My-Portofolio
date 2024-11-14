d9c5d5c20e11d3f9adda47e64fd52b137d46ffd6
dhevasm <dhevasm2019@gmail.com>
2024-11-14 18:16:11 +0700

    Project page min-h-screen

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -25,7 +25,7 @@ const ProjectList = () => {
   return (
     <section
       id="portfolio"
-      className="relative bg-gradient-to-br from-black to-gray-900 text-white py-20 md:py-32"
+      className="relative bg-gradient-to-br from-black to-gray-900 text-white py-20 md:py-32 min-h-screen"
     >
       <Particle />
       <div className="max-w-6xl mx-auto px-4 z-10">
